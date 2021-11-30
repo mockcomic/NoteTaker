9bfb063d1d590d22f2db2e24a9dd8194c5d1a793
Kurtis <gears2525@aol.com>
2021-11-29 22:31:23 -0800

    moved files out of develop

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const router = require('express').Router();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,15 +9,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes',(req,res) => {
-    res.sendFile(path.join(__dirname + '/Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
-
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + '/Develop/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`API server now on port http://localhost:${PORT}/`);
