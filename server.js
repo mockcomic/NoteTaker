44b3df398f8e0bf3537fc5b36d02dcfa813841bb
Kurtis <gears2525@aol.com>
2021-11-29 23:24:03 -0800

    created routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const router = require('express').Router();
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoutes = require('./routes/htmlRoutes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
