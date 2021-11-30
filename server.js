65d8c0c51cd5e622c1c9595a5a7a73e67d322987
Kurtis <gears2525@aol.com>
2021-11-29 22:18:32 -0800

    Created server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname + '/Develop/public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname + '/Develop/public/index.html'));
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port http://localhost:${PORT}/`);
+});
