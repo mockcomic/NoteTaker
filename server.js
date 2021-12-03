4b73fd0909145a801b48d6674f211c91ff8662ef
Kurtis <gears2525@aol.com>
2021-12-02 16:12:40 -0800

    updated routes

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const path = require('path');
 const router = require('express').Router();
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api',apiRoutes)
+app.use('/', htmlRoutes);
 
-app.get('/notes',(req,res) => {
-    res.sendFile(path.join(__dirname + '/public/notes.html'));
-});
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+// app.get('/notes',(req,res) => {
+//     res.sendFile(path.join(__dirname + '/public/notes.html'));
+// });
+
+// app.get('/api/notes', (req,res) => {
+//   res.sendFile(path.join(__dirname + '/db/db.json'));
+// });
+
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname + '/public/index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`API server now on port http://localhost:${PORT}/`);
